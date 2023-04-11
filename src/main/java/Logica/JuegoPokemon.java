35355e868c8c1b5ae5d42acd6c69bd940ac55f11
MarieM <ingmariemelendez@gmail.com>
2023-04-10 23:15:33 -0300

    Se carga el proyecto de pokemon juego como ejercicio de java.

---
+++ src/main/java/Logica/JuegoPokemon.java
@@ -0,0 +1,23 @@
+package Logica;
+
+public class JuegoPokemon {
+
+    public static void main(String[] args){
+
+        Squirtle squirtle = new Squirtle();
+        Charmander charmander = new Charmander();
+        Bulbasaur bulbasaur = new Bulbasaur();
+        Pikachu pikachu = new Pikachu();
+
+        squirtle.atacarAraniazo();
+        squirtle.atacarHidrobomba();
+        charmander.atacarAraniazo();
+        charmander.atacarLanzallamas();
+        bulbasaur.atacarAraniazo();
+        bulbasaur.atacarDrenaje();
+        pikachu.atacarAraniazo();
+        pikachu.atacarImpactrueno();
+
+    }
+
+}
